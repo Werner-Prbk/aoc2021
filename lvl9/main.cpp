0736a7d6312bf45527a77b28177d5f43e6740011
breitwieser.werner <breitwieser.werner@fronius.com>
2021-12-09 21:20:49 +0100

    cleanup

--- lvl9/main.cpp
+++ lvl9/main.cpp
@@ -3,6 +3,7 @@
 #include <locale>
 #include <algorithm>
 #include <numeric>
+#include <functional>
 #include "../common/aoc.h"
 
 using namespace std;
@@ -27,16 +28,11 @@ vector<Point*> GetAdjacents(vector<vector<Point>>& map, int row, int col)
     return p;
 }
 
-void FindLowPoints(vector<vector<Point>>& map)
+void IterateOverMap(vector<vector<Point>> &map, function<void(vector<vector<Point>> &map, int row, int col)> op)
 {
     for(size_t r = 0; r < map.size(); ++r){
         for (size_t c = 0; c < map[0].size(); ++c){
-            auto adj = GetAdjacents(map, r, c);
-            auto min = min_element(adj.begin(), adj.end(), [](auto const&lhs, auto const&rhs) {return lhs->value < rhs->value;});
-            if (map[r][c].value < (*min)->value)
-            {
-                map[r][c].isLowPoint = true;
-            }
+            op(map, r, c);
         }
     }
 }
@@ -55,55 +51,53 @@ int main(){
         map.push_back(v);
     });
 
-    FindLowPoints(map);
+    IterateOverMap(map, [](auto& map, int r, int c) {
+        auto adj = GetAdjacents(map, r, c);
+        auto min = min_element(adj.begin(), adj.end(), [](auto const&lhs, auto const&rhs) {return lhs->value < rhs->value;});
+        if (map[r][c].value < (*min)->value)
+        {
+            map[r][c].isLowPoint = true;
+        }
+    });
 
     // for part 2
     auto basinId = 0;
-
     auto riskLevelSum = 0;
 
-    for(size_t r = 0; r < map.size(); ++r){
-        for (size_t c = 0; c < map[0].size(); ++c){
-            if (map[r][c].isLowPoint)
-            {
-                riskLevelSum += (1 + map[r][c].value);
+    IterateOverMap(map, [&riskLevelSum, &basinId](auto& map, int r, int c){
+        if (map[r][c].isLowPoint) {
+            riskLevelSum += (1 + map[r][c].value);
 
-                // for part 2
-                map[r][c].basinId = basinId++;
-            }
+            // for part 2
+            map[r][c].basinId = basinId++;
         }
-    }
+    });
     
     cout << "Risk level sum: " << riskLevelSum << endl; // 550
     
     // Part 2
     vector<int> basinIdSizes (basinId, 1);
     bool found = false;
-    do
-    {
+    do {
         found = false;
-        for(size_t r = 0; r < map.size(); ++r){
-            for (size_t c = 0; c < map[0].size(); ++c){
-                if (map[r][c].basinId < 0 && 
-                    map[r][c].value < 9) {
-                    auto adj = GetAdjacents(map, r, c);
-                    for (auto &&a : adj)
-                    {
-                        if (a->basinId >= 0)
-                        {
-                            map[r][c].basinId = a->basinId;
-                            basinIdSizes[a->basinId]++;
-                            found = true;
-                            break;
-                        }
+        IterateOverMap(map, [&basinIdSizes, &found](auto& map, int r, int c){
+            if (map[r][c].basinId < 0 && map[r][c].value < 9) {
+                auto adj = GetAdjacents(map, r, c);
+                for (auto &&a : adj) {
+                    if (a->basinId >= 0) {
+                        map[r][c].basinId = a->basinId;
+                        basinIdSizes[a->basinId]++;
+                        found = true;
+                        break;
                     }
                 }
             }
-        }
+        });
     } while (found);
     
     sort(basinIdSizes.begin(), basinIdSizes.end(), greater<int>());
     auto result = accumulate(basinIdSizes.begin(), basinIdSizes.begin() + 3, 1, multiplies<int>());
+    
     cout << "Product of 3 lagest basin sizes: " << result << endl;   // 1100682
 
     return 0;
