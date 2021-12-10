9030371be9447e8b52e6c8aa37b2848248b06fb5
breitwieser.werner <breitwieser.werner@fronius.com>
2021-12-10 10:31:10 +0100

    lvl10 part 2

--- lvl10/main.cpp
+++ lvl10/main.cpp
@@ -3,65 +3,22 @@
 #include <map>
 #include <stack>
 #include <limits>
+#include <algorithm>
 #include "../common/aoc.h"
 
 using namespace std;
 using namespace aoc;
 
-class SyntaxChecker 
-{
-    public:
-    bool Process(char c)
-    {
-        switch (c)
-        {
-            case '(': _counter['(']++; break;
-            case ')': _counter['(']--; break;
-            case '[': _counter['[']++; break;
-            case ']': _counter['[']--; break;
-            case '{': _counter['{']++; break;
-            case '}': _counter['{']--; break;
-            case '<': _counter['<']++; break;
-            case '>': _counter['<']--; break;
-            default: throw "Unexpected token";
-        }
-
-        _processCnt++;
-
-
-    }
-
-    private:
-
-
-
-    int _processCnt {0};
-    map<char, int> _counter {{'(', 0}, {'[', 0}, {'<', 0}, {'{', 0}};
-};
-
-
-bool isOpening(char c)
-{
-    return 
-        c == '(' ||
-        c == '[' ||
-        c == '{' ||
-        c == '<';
+bool isOpening(char c) {
+    return c == '(' || c == '[' || c == '{' || c == '<';
 }
 
-bool isClosing(char c)
-{
-    return 
-        c == ')' ||
-        c == ']' ||
-        c == '}' ||
-        c == '>';
+bool isClosing(char c) {
+    return c == ')' || c == ']' || c == '}' || c == '>';
 }
 
-char getMatchingClosing(char c)
-{
-    switch (c)
-    {
+char getMatchingClosing(char c) {
+    switch (c) {
         case '(': return ')';
         case '[': return ']';
         case '{': return '}';
@@ -70,18 +27,37 @@ char getMatchingClosing(char c)
     }
 }
 
+int getSyntaxErrorScore(char c) {
+    switch (c) {
+        case ')': return 3;
+        case ']': return 57;
+        case '}': return 1197;
+        case '>': return 25137;
+        default: throw "Unexpected!";
+    }
+}
+
+int getAutoCompletionScore(char c) {
+    switch (c) {
+        case ')': return 1;
+        case ']': return 2;
+        case '}': return 3;
+        case '>': return 4;
+        default: throw "Unexpected!";
+    }
+}
+
 struct syntaxError {
     size_t column {0};
     bool hasError {false};
     bool isComplete {false};
+    vector<char> autoCompleted {};
 };
 
-syntaxError checkLineSyntax(string const& line)
-{
+syntaxError checkLineSyntax(string const& line) {
     stack<char> openingStack;
 
-    for(size_t i = 0; i < line.size(); ++i)
-    {
+    for(size_t i = 0; i < line.size(); ++i) {
         if (isOpening(line[i])){
             openingStack.push(line[i]);
         }
@@ -92,42 +68,50 @@ syntaxError checkLineSyntax(string const& line)
             openingStack.pop();
         }
     }
+    
+    auto se = syntaxError{0, false, openingStack.size() == 0};
 
-    return syntaxError{0, false, openingStack.size() == 0};
-}
-
-int getScore(char c)
-{
-    switch (c)
-    {
-        case ')': return 3;
-        case ']': return 57;
-        case '}': return 1197;
-        case '>': return 25137;
-        default: throw "Unexpected!";
+    // part 2
+    while (openingStack.size() > 0) {
+        se.autoCompleted.push_back(getMatchingClosing(openingStack.top()));
+        openingStack.pop();
     }
+    
+    return se;
 }
 
-int main(){
+int main() {
     vector<string> lines;
     readInput("input.txt", [&lines](auto const& line){ lines.push_back(line); });
 
-    uint score = 0;
+    uint syntaxErrorScore = 0;
 
-    for (auto &&l : lines)
-    {
+    vector <syntaxError> syntaxErrors;
+
+    for (auto &&l : lines) {
         auto se = checkLineSyntax(l);
+        syntaxErrorScore += se.hasError ? getSyntaxErrorScore(l[se.column]) : 0;
+        syntaxErrors.push_back(se);
+    }
 
-        if (se.hasError)
-        {
-            //cout << l[se.column] << " on column " << se.column << endl;
-            score += getScore(l[se.column]);
+    cout << "Total syntax error score: " << syntaxErrorScore << endl;
+
+    // part 2
+    vector<uint64_t> autoCompletionScores;
+    for (auto &&s : syntaxErrors) {
+        if (!s.hasError) {
+            uint64_t score = 0;
+            for (auto &&c : s.autoCompleted) {
+                score *= 5;
+                score += getAutoCompletionScore(c);
+            }
+            autoCompletionScores.push_back(score);
         }
     }
 
-    cout << "Total score: " << score << endl;
-    
+    sort(autoCompletionScores.begin(), autoCompletionScores.end());
 
+    cout << "Winner for auto completion is: " << autoCompletionScores[autoCompletionScores.size() / 2] << endl;
 
     return 0;
 }
